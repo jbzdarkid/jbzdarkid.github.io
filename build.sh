bb456c7807a6baac9ef13c664aec0e85af785f6e
jbzdarkid <jbzdarkid@users.noreply.github.com>
2016-06-15 08:23:01 -0400

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-python scraper.py
-ls
-git config user.name "Travis CI"
-git config user.email "<>"
-git config push.default simple
-git add feed.rss
-git push "https://${GH_TOKEN}@github.com/jbzdarkid/jbzdarkid.github.io.git" master:master
