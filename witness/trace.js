6a850dc41199bed5c76b3f83dcb74f0fecb6a8c7
jbzdarkid <jbzdarkid@gmail.com>
2017-03-15 10:33:03 -0400

    Swapped X and Y so X<->width and Y<->height
    Next, I need to reverse the ids so this reads better.

--- witness/trace.js
+++ witness/trace.js
@@ -5,8 +5,8 @@ function trace(elem) {
   var height = parseInt(window.getComputedStyle(parent).height)
   data = {
     'table':parent.id.split('_')[0],
-    'x':parseInt(parent.id.split('_')[1]),
-    'y':parseInt(parent.id.split('_')[2]),
+    'x':parseInt(parent.id.split('_')[2]),
+    'y':parseInt(parent.id.split('_')[1]),
     'subx':width/2,
     'suby':height/2,
     }
@@ -81,8 +81,8 @@ function _draw(elem, subx, suby, draw_rect) {
     circ.style.stroke = 'black'
     circ.style.strokeOpacity = '0.3'
   }
-  circ.style.cx = suby
-  circ.style.cy = subx
+  circ.style.cx = subx
+  circ.style.cy = suby
   if (draw_rect) {
     rect.style.height = 0
     rect.style.width = 0
@@ -96,38 +96,38 @@ function _draw(elem, subx, suby, draw_rect) {
     } else if (exit_dir == null) {
       // Still in this segment, draw a partial
       if (enter_dir == 'r') {
+        rect.style.width = subx
         rect.style.height = height
-        rect.style.width = suby
       } else if (enter_dir == 'l') {
+        rect.style.width = width - subx
         rect.style.height = height
-        rect.style.width = width - suby
-        rect.setAttribute('transform', 'translate('+suby+', 0)')
+        rect.setAttribute('transform', 'translate('+subx+', 0)')
       } else if (enter_dir == 'd') {
-        rect.style.height = subx
         rect.style.width = width
+        rect.style.height = suby
       } else if (enter_dir == 'u') {
-        rect.style.height = height - subx
         rect.style.width = width
-        rect.setAttribute('transform', 'translate(0, '+subx+')')
+        rect.style.height = height - suby
+        rect.setAttribute('transform', 'translate(0, '+suby+')')
       }
     } else if (enter_dir == exit_dir) {
       // Passed through in a straight line, fill the entirety
-      rect.style.height = height
       rect.style.width = width
+      rect.style.height = height
     } else {
       // Passed through in a corner, create as such
-      rect.style.rx = height
-      rect.style.height = height*2
+      rect.style.rx = width
       rect.style.width = width*2
+      rect.style.height = height*2
       var x_trans = 0
       var y_trans = 0
-      if (enter_dir == 'r' || exit_dir == 'l') {
-        x_trans = -height
-      }
       if (enter_dir == 'd' || exit_dir == 'u') {
-        y_trans = -width
+        x_trans = -width
       }
-      rect.setAttribute('transform', 'translate('+x_trans+', '+y_trans+')')
+      if (enter_dir == 'r' || exit_dir == 'l') {
+        y_trans = -height
+      }
+      rect.setAttribute('transform', 'translate('+y_trans+', '+x_trans+')')
     }
     svg.appendChild(rect)
   }
@@ -137,143 +137,142 @@ function _draw(elem, subx, suby, draw_rect) {
 
 function onMouseMove(e) {
   var sens = 0.3
-  // Caution: reversed
-  var dx = sens*(e.movementY || e.mozMovementY || e.webkitMovementY || 0)
-  var dy = sens*(e.movementX || e.mozMovementX || e.webkitMovementX || 0)
-  data.subx += dx
-  data.suby += dy
-  var elem = document.getElementById(data.table+'_'+data.x+'_'+data.y)
+  var dx = e.movementX || e.mozMovementX || e.webkitMovementX || 0
+  var dy = e.movementY || e.mozMovementY || e.webkitMovementY || 0
+  data.subx += dx*sens
+  data.suby += dy*sens
+  var elem = document.getElementById(data.table+'_'+data.y+'_'+data.x)
   var width = parseInt(window.getComputedStyle(elem).width)
   var height = parseInt(window.getComputedStyle(elem).height)
 
-  // The *ahem* corner case
+  // FIXME: Can I just use this for all collision detection? If so, eliminates the need for dx?
+  // Calculates the distance to the edge in each direction
   if (elem.className.includes('corner')) {
-    var abs_x = (data.subx < height/2) ? data.subx : height - data.subx
-    var abs_y = (data.suby < width/2) ? data.suby : width - data.suby
-    // Only take action on the more active direction
-    if (abs_x > abs_y) {
+    var dist_x = (data.subx < width/2) ? data.subx : width - data.subx
+    var dist_y = (data.suby < height/2) ? data.suby : height - data.suby
+    // Reduce the larger distance to 0
+    if (dist_x > dist_y) { // FIXME: Reverse?
       if (data.subx - 11 < 0) data.subx = 11
-      if (data.subx + 11 > height) data.subx = height - 11
+      if (data.subx + 11 > width) data.subx = width - 11
     } else {
       if (data.suby - 11 < 0) data.suby = 11
-      if (data.suby + 11 > width) data.suby = width - 11
+      if (data.suby + 11 > height) data.suby = height - 11
     }
   }
 
   // Collision detection
   if (dx < 0 && data.subx - 11 < 0) {
-    var new_elem = document.getElementById(data.table+'_'+(data.x-1)+'_'+data.y)
+    var new_elem = document.getElementById(data.table+'_'+data.y+'_'+(data.x-1))
     if (new_elem == null) {
       data.subx = 11
-    } else if (!(new_elem.className.endsWith('trace') ||elem.className.endsWith('trace-d'))) {
+    } else if (!(new_elem.className.endsWith('trace') ||elem.className.endsWith('trace-r'))) {
       data.subx = 11
     }
-  } else if (dx > 0 && data.subx + 11 > height) {
-    var new_elem = document.getElementById(data.table+'_'+(data.x+1)+'_'+data.y)
+  } else if (dx > 0 && data.subx + 11 > width) {
+    var new_elem = document.getElementById(data.table+'_'+data.y+'_'+(data.x+1))
     if (new_elem == null) {
-      data.subx = height - 11
-    } else if (!(new_elem.className.endsWith('trace') ||elem.className.endsWith('trace-u'))) {
-      data.subx = height - 11
+      data.subx = width - 11
+    } else if (!(new_elem.className.endsWith('trace') ||elem.className.endsWith('trace-l'))) {
+      data.subx = width - 11
     }
   }
   if (dy < 0 && data.suby - 11 < 0) {
-    var new_elem = document.getElementById(data.table+'_'+data.x+'_'+(data.y-1))
+    var new_elem = document.getElementById(data.table+'_'+(data.y-1)+'_'+data.x)
     if (new_elem == null) {
       data.suby = 11
-    } else if (!(new_elem.className.endsWith('trace') ||elem.className.endsWith('trace-r'))) {
+    } else if (!(new_elem.className.endsWith('trace') ||elem.className.endsWith('trace-d'))) {
       data.suby = 11
     }
-  } else if (dy > 0 && data.suby + 11 > width) {
-    var new_elem = document.getElementById(data.table+'_'+data.x+'_'+(data.y+1))
+  } else if (dy > 0 && data.suby + 11 > height) {
+    var new_elem = document.getElementById(data.table+'_'+(data.y+1)+'_'+data.x)
     if (new_elem == null) {
-      data.suby = width - 11
-    } else if (!(new_elem.className.endsWith('trace') ||elem.className.endsWith('trace-l'))) {
-      data.suby = width - 11
+      data.suby = height - 11
+    } else if (!(new_elem.className.endsWith('trace') ||elem.className.endsWith('trace-u'))) {
+      data.suby = height - 11
     }
   }
 
   // Redraw all elements near the cursor
   for (var x=-1; x<=1; x++) {
     for (var y=-1; y<=1; y++) {
-      var temp_elem = document.getElementById(data.table+'_'+(data.x+x)+'_'+(data.y+y))
+      var temp_elem = document.getElementById(data.table+'_'+(data.y+y)+'_'+(data.x+x))
       if (temp_elem == null) continue
       var temp_width = width
       var temp_height = height
       if (x == -1) {
-        temp_height = parseInt(window.getComputedStyle(temp_elem).height)
-      }
-      if (y == -1) {
         temp_width = parseInt(window.getComputedStyle(temp_elem).width)
       }
+      if (y == -1) {
+        temp_height = parseInt(window.getComputedStyle(temp_elem).height)
+      }
       if (x == 0 || y == 0) {
-        _draw(temp_elem, data.subx - x * temp_height, data.suby - y * temp_width, true)
+        _draw(temp_elem, data.subx - x * temp_width, data.suby - y * temp_height, true)
       } else {
-        _draw(temp_elem, data.subx - x * temp_height, data.suby - y * temp_width, false)
+        _draw(temp_elem, data.subx - x * temp_width, data.suby - y * temp_height, false)
       }
     }
   }
 
-  // FIXME: Something like traced-up-right (for a corner) and traced-down-down (for an edge)
   // Generic movement
-  if (data.subx < 0) { // Moving up
-    var new_elem = document.getElementById(data.table+'_'+(data.x-1)+'_'+data.y)
+  if (data.subx < 0) { // Moving left
+    var new_elem = document.getElementById(data.table+'_'+data.y+'_'+(data.x-1))
     if (new_elem != null) {
-      var new_height = parseInt(window.getComputedStyle(new_elem).height)
-      if (new_elem.className.endsWith('trace')) { // Trace new path
+      var new_width = parseInt(window.getComputedStyle(new_elem).width)
+      if (new_elem.className.endsWith('trace')) { // Traced new path
         data.x--
-        data.subx += new_height
-        elem.className += '-u'
-        new_elem.className += '-u'
-      } else if (elem.className.endsWith('-d')) { // Retrace path
+        data.subx += new_width
+        elem.className += '-l'
+        new_elem.className += '-l'
+      } else if (elem.className.endsWith('-r')) { // Retraced path
         data.x--
-        data.subx += new_height
+        data.subx += new_width
         elem.className = elem.className.substring(0, elem.className.length-2)
         new_elem.className = new_elem.className.substring(0, new_elem.className.length-2)
       }
     }
-  } else if (data.subx > height) { // Moving down
-    var new_elem = document.getElementById(data.table+'_'+(data.x+1)+'_'+data.y)
+  } else if (data.subx > width) { // Moving right
+    var new_elem = document.getElementById(data.table+'_'+data.y+'_'+(data.x+1))
     if (new_elem != null) {
       if (new_elem.className.endsWith('trace')) { // Traced new path
         data.x++
-        data.subx -= height
-        elem.className += '-d'
-        new_elem.className += '-d'
-      } else if (elem.className.endsWith('-u')) { // Retraced path
+        data.subx -= width
+        elem.className += '-r'
+        new_elem.className += '-r'
+      } else if (elem.className.endsWith('-l')) { // Retraced path
         data.x++
-        data.subx -= height
+        data.subx -= width
         elem.className = elem.className.substring(0, elem.className.length-2)
         new_elem.className = new_elem.className.substring(0, new_elem.className.length-2)
       }
     }
   }
-  if (data.suby < 0) { // Moving left
-    var new_elem = document.getElementById(data.table+'_'+data.x+'_'+(data.y-1))
+  if (data.suby < 0) { // Moving up
+    var new_elem = document.getElementById(data.table+'_'+(data.y-1)+'_'+data.x)
     if (new_elem != null) {
-      var new_width = parseInt(window.getComputedStyle(new_elem).width)
-      if (new_elem.className.endsWith('trace')) { // Traced new path
+      var new_height = parseInt(window.getComputedStyle(new_elem).height)
+      if (new_elem.className.endsWith('trace')) { // Trace new path
         data.y--
-        data.suby += new_width
-        elem.className += '-l'
-        new_elem.className += '-l'
-      } else if (elem.className.endsWith('-r')) { // Retraced path
+        data.suby += new_height
+        elem.className += '-u'
+        new_elem.className += '-u'
+      } else if (elem.className.endsWith('-d')) { // Retrace path
         data.y--
-        data.suby += new_width
+        data.suby += new_height
         elem.className = elem.className.substring(0, elem.className.length-2)
         new_elem.className = new_elem.className.substring(0, new_elem.className.length-2)
       }
     }
-  } else if (data.suby > width) { // Moving right
-    var new_elem = document.getElementById(data.table+'_'+data.x+'_'+(data.y+1))
+  } else if (data.suby > height) { // Moving down
+    var new_elem = document.getElementById(data.table+'_'+(data.y+1)+'_'+data.x)
     if (new_elem != null) {
       if (new_elem.className.endsWith('trace')) { // Traced new path
         data.y++
-        data.suby -= width
-        elem.className += '-r'
-        new_elem.className += '-r'
-      } else if (elem.className.endsWith('-l')) { // Retraced path
+        data.suby -= height
+        elem.className += '-d'
+        new_elem.className += '-d'
+      } else if (elem.className.endsWith('-u')) { // Retraced path
         data.y++
-        data.suby -= width
+        data.suby -= height
         elem.className = elem.className.substring(0, elem.className.length-2)
         new_elem.className = new_elem.className.substring(0, new_elem.className.length-2)
       }
