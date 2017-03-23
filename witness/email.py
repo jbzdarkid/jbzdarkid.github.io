673f0ced173c25bc772704361957cc84f2a5d359
jbzdarkid <jbzdarkid@gmail.com>
2017-03-23 14:26:57 -0400

    Real AES encryption, though I don't think emails are working

--- witness/email.py
+++ witness/email.py
@@ -1,28 +1,38 @@
 from selenium.webdriver import PhantomJS
-from json import load
 from os import getcwd, environ
-import smtplib
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+from smtplib import SMTP
 
 driver = PhantomJS()
 driver.set_window_size(1280, 720)
-print getcwd()+'index.html'
-driver.get(getcwd()+'index.html')
+print getcwd()+'/witness/index.html'
+driver.get(getcwd()+'/witness/index.html')
 driver.save_screenshot('temp.png')
-system('gpg whatever')
+
+sha = SHA256.new()
+sha.update(environ['PASSWORD'])
+key = sha.hexdigest()[:AES.block_size*2]
+text = open('emails.txt', 'rb').read()
+iv = text[:AES.block_size]
+cipher = text[AES.block_size:]
+aes = AES.new(key, AES.MODE_CBC, iv)
+plain = aes.decrypt(cipher)
 
 FROM = environ['USERNAME']
-TO = load('emails.txt')
-msg = MimeMultipart()
-msg['From'] = FROM
-msg['To'] = ', '.join(TO)
-msg['Date'] = 'blah'
-msg['Subject'] = 'Witness puzzle for blah'
-msg.attach(MIMEImage(open('temp.png', 'rb').read()))
-
-server = smtplib.SMTP('smtp.gmail.com', 587)
+server = SMTP('smtp.gmail.com', 587)
 server.ehlo()
 server.starttls()
 server.login(environ['USERNAME'], environ['PASSWORD'])
-server.sendmail(FROM, TO, msg.as_string())
-server.quit()
 
+for to in plain.split('\n'):
+	to = to.strip()
+	msg = MimeMultipart()
+	msg['From'] = FROM
+	msg['To'] = ', '.join(TO)
+	msg['Date'] = 'blah'
+	msg['Subject'] = 'Witness puzzle for blah'
+	msg.attach(MIMEImage(open('temp.png', 'rb').read()))
+	server.sendmail(FROM, TO, msg.as_string())
+
+server.quit()
