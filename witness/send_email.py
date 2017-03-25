33c4cc3902a145620d22afb6830cf71438f0e564
jbzdarkid <jbzdarkid@gmail.com>
2017-03-25 00:19:22 -0400

    This is probably the issue

--- witness/send_email.py
+++ witness/send_email.py
@@ -29,7 +29,7 @@ server.ehlo()
 server.starttls()
 server.login(FROM, environ['PASSWORD'])
 
-text = '<a href="jbzdarkid.github.io/index.html#1><img src="data:image/png;base64,%s"></a>' % b64encode(open('temp.png', 'rb').read())
+text = '<html><body><a href="jbzdarkid.github.io/index.html#1><img src="data:image/png;base64,%s"></a></body></html>' % b64encode(open('temp.png', 'rb').read())
 
 for TO in plain.split(','):
 	msg = MIMEText(text)
