6dd1b655456e2a8519d6767549932f4423ad7fba
jbzdarkid <jbzdarkid@users.noreply.github.com>
2017-09-09 04:24:50 -0700

    re-adding old code
    should post a red square?

--- witness/worker.js
+++ witness/worker.js
@@ -4,6 +4,5 @@ onmessage = function(e) {
   div.style.height = '100px'
   div.style.width = '100px'
   div.style.background = 'red'
-  console.log('Posting message back to main script');
-  postMessage(div)
+  document.appendChild(div)
 }
