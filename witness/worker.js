9395abf0911bf28e4741d0d8b8cb0e92c400c6dc
jbzdarkid <jbzdarkid@users.noreply.github.com>
2017-09-09 04:53:26 -0700

    turning up time to verify

--- witness/worker.js
+++ witness/worker.js
@@ -1,7 +1,7 @@
 onmessage = function() {
   console.log('Recieved at worker')
-  var N = 10000000		
-  for (var i=0; i<N; i++) {		
+  var N = 100000000
+  for (var i=0; i<N; i++) {
     var j = i**i
   }
   postMessage(null)
