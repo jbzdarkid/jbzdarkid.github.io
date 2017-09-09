cae427501ef16a5df034ac2c50f2efe7207b1c09
jbzdarkid <jbzdarkid@users.noreply.github.com>
2017-09-09 05:53:43 -0700

    looks like scope is important

--- witness/temp.js
+++ witness/temp.js
@@ -1,6 +1,6 @@
-onmessage = function(e) {
+window.onmessage = function(e) {
   console.log('Recieved at worker')
-  var N = 100000001
+  var N = 100000000
   for (var i=0; i<N; i++) {
     var j = i**i
   }
