5e889344a38336a4f24fbbc63ae86f7c183167cd
jbzdarkid <jbzdarkid@users.noreply.github.com>
2017-09-09 04:38:13 -0700

    document is inaccessible in the worker

--- witness/worker.js
+++ witness/worker.js
@@ -1,7 +1,6 @@
-onmessage = function(e) {
-  var div = document.createElement('div')
+onmessage = function(div) {
   div.style.height = '100px'
   div.style.width = '100px'
   div.style.background = 'red'
-  document.appendChild(div)
+  postMessage(div)
 }
