a731f1adaa5ab17977d40986f1341a93ceaaa64a
jbzdarkid <jbzdarkid@users.noreply.github.com>
2017-09-09 04:01:20 -0700

    testing workers
    I don't know if they can do async graphics calls, I hope so

---
+++ witness/temp2.js
@@ -0,0 +1,7 @@
+function onmessage(e) {
+  var div = document.createElement('div')
+  div.style.height = '100px'
+  div.style.width = '100px'
+  div.style.background = 'red'
+  document.body.appendChild(div)
+}
