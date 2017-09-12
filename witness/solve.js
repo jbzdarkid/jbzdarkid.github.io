9d4595ed32272d90b7fe12188092129190eb6595
jbzdarkid <jbzdarkid@gmail.com>
2017-09-12 16:56:14 -0700

    Try to fix multithreading issue

--- witness/solve.js
+++ witness/solve.js
@@ -1,5 +1,7 @@
 onmessage = function(e) {
   var puzzle = e.data
+  console.log(puzzle)
+  console.log(puzzle.isEndpoint)
   var solutions = []
   solve(puzzle, {'x':puzzle.start.x, 'y':puzzle.start.y}, solutions)
   postMessage(solutions[_randint(solutions.length)])
