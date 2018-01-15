e19a9e3d8b8d111ccdbee30c02cdb0cdebc0c7f9
jbzdarkid <jbzdarkid@users.noreply.github.com>
2018-01-15 14:38:08 -0800

    Added talos basic stuff (moved from webs)

---
+++ talos/autosize.js
@@ -0,0 +1,12 @@
+console.log('<1>@autosize.js');
+// Auto-size images to fit to screen width
+function resize(document) {
+  var width = document.body.clientWidth/5-20;
+  console.log('Computed img width:', width);
+  var img_tags = document.getElementsByTagName('img')
+  for (var i=0; i<img_tags.length; i++) {
+    img_tags[i].width = width;
+  }
+}
+window.onload = function() {resize(document)};
+window.onresize = function() {resize(document)};
