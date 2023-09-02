09d680436329942d64d25fb5d6de5fd4397b10ab
jbzdarkid <jbzdarkid@gmail.com>
2023-09-02 15:16:09 -0700

    Small fixes

--- .github/workflows/validate.py
+++ .github/workflows/validate.py
@@ -23,13 +23,13 @@ def gpg_encrypt(plaintext, key):
         return f.read().hex()
 
 print('Validating puzzle...')
-contents = open('.github/workflows/template_validate.html', 'r', encoding='utf-8').read()
+puzzle_list = open('.github/workflows/template_validate.html', 'r', encoding='utf-8').read()
 puzzle = os.environ['PUZZLE']
-contents = contents.replace('%input_data%', puzzle) # Let javascript do the object load; we'll be happy with whatever.
+puzzle_list = puzzle_list.replace('%input_data%', puzzle) # Let javascript do the object load; we'll be happy with whatever.
 
 tempfile = Path('temp.html').resolve()
 with tempfile.open('w', encoding='utf-8') as f:
-    f.write(contents)
+    f.write(puzzle_list)
 
 args = ['google-chrome-stable', tempfile.as_uri(), '--headless=new', '--dump-dom']
 dom = subprocess.run(args, text=True, encoding='utf-8', stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, check=True).stdout
@@ -54,15 +54,19 @@ solution_path = gpg_encrypt(solution_path, os.environ['SECRET'])
 
 # This is a slightly updated display_hash solution -- rather than hashing the puzzle, I'm just generating a random ID every time.
 # (Also, I'm flattening the alphabet ahead of time to avoid letter bias.)
-alphabet = '23456789ABCDEFGHJKLMNPQRSTUVWXYZ' # Everything but 0, O, 1, I
+# Alphabet of size 32: [0-9A-Z] / [I1O0]
+# 8 characters at 5 bits each = 40 bits = 2^40 words
+# 50% of collision if I ever get 2^20 (~1,000,000) puzzles
+alphabet = '23456789ABCDEFGHJKLMNPQRSTUVWXYZ'
 display_hash = ''.join(random.choices(alphabet, k=8))
 image_url = f'images/{display_hash}.png'
 page_url = f'play/{display_hash}.html'
 
-print('Creating puzzle page...')
+print('Saving puzzle image...')
 with open(image_url, 'xb') as f:
     f.write(img_bytes)
 
+print('Creating puzzle page...')
 contents = open('.github/workflows/template_play.html', 'r', encoding='utf-8').read()
 contents = contents \
     .replace('%title%', title) \
