6bc68608a9a5e5c6fbe64f60fb9862134d72d9d6
jbzdarkid <jbzdarkid@gmail.com>
2023-09-03 18:16:50 -0700

    No more application

--- .github/workflows/convert.py
+++ .github/workflows/convert.py
@@ -32,42 +32,41 @@ def get_puzzles(order='asc', offset=0, limit=100):
     return r.json()
 
 if __name__ == '__main__':
-    with application.app_context():
-        offset = 0 if len(sys.argv) < 1 else int(sys.argv[1])
-        limit = 100 if len(sys.argv) < 2 else int(sys.argv[2])
-        puzzles = get_puzzles(offset, limit)
-        puzzle_list = open('puzzle_list.js', 'r', encoding='utf-8').read().split('\n')
+    offset = 0 if len(sys.argv) < 1 else int(sys.argv[1])
+    limit = 100 if len(sys.argv) < 2 else int(sys.argv[2])
+    puzzles = get_puzzles(offset, limit)
+    puzzle_list = open('puzzle_list.js', 'r', encoding='utf-8').read().split('\n')
 
-        for data in puzzles:
-            # Extract data from the data (in case we fail, not that we should)
-            title = data['title']
-            puzzle_json = data['puzzle_json']
-            solution_path = json.dumps(json.loads(data['solution_json'])['path'])
+    for data in puzzles:
+        # Extract data from the data (in case we fail, not that we should)
+        title = data['title']
+        puzzle_json = data['puzzle_json']
+        solution_path = json.dumps(json.loads(data['solution_json'])['path'])
 
-            # Encrypt this since we'll be saving it directly on the page
-            solution_path = gpg_encrypt(solution_path, os.environ['SECRET'])
+        # Encrypt this since we'll be saving it directly on the page
+        solution_path = gpg_encrypt(solution_path, os.environ['SECRET'])
 
-            display_hash = data['display_hash']
-            image_url = f'images/{display_hash}.png'
-            page_url = f'play/{display_hash}.html'
+        display_hash = data['display_hash']
+        image_url = f'images/{display_hash}.png'
+        page_url = f'play/{display_hash}.html'
 
-            print('Downloading image...')
-            urllib.request.urlretrieve(data['url'], image_url)
+        print('Downloading image...')
+        urllib.request.urlretrieve(data['url'], image_url)
 
-            print('Creating puzzle page...')
-            contents = open('.github/workflows/template_play.html', 'r', encoding='utf-8').read()
-            contents = contents \
-                .replace('%title%', title) \
-                .replace('%display_hash%', display_hash) \
-                .replace('%image_url%', image_url) \
-                .replace('%puzzle%', puzzle_json) \
-                .replace('%solution%', solution_path)
-            with open(page_url, 'x', encoding='utf-8') as f:
-                f.write(contents)
+        print('Creating puzzle page...')
+        contents = open('.github/workflows/template_play.html', 'r', encoding='utf-8').read()
+        contents = contents \
+            .replace('%title%', title) \
+            .replace('%display_hash%', display_hash) \
+            .replace('%image_url%', image_url) \
+            .replace('%puzzle%', puzzle_json) \
+            .replace('%solution%', solution_path)
+        with open(page_url, 'x', encoding='utf-8') as f:
+            f.write(contents)
 
-            puzzle_list.insert(1, f'"{display_hash}{title}",')
+        puzzle_list.insert(1, f'"{display_hash}{title}",')
 
-        with open('puzzle_list.js', 'w', encoding='utf-8') as f:
-            f.write('\n'.join(puzzle_list))
+    with open('puzzle_list.js', 'w', encoding='utf-8') as f:
+        f.write('\n'.join(puzzle_list))
 
 
