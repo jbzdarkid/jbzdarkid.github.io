c3f47adc534d0de355b696f23f3a84522b17fbce
Validate and publish workflow <jbzdarkid@users.noreply.github.com>
2025-05-12 13:33:19 +0000

    Published puzzle QYJXSUN7

--- puzzle_list.js
+++ puzzle_list.js
@@ -1,5 +1,5 @@
 window.puzzleList = [
-"QYJXSUN7Unnamed Puzzle",
+"QYJXSUN7Madness",
 "7EN5LUBFUnnamed Puzzle",
 "R63N3WT3Unnamed Pillar (R Symmetry) Puzzle",
 "GR7EHV63Skipped Variety May 2025 Mountainside Vault",
