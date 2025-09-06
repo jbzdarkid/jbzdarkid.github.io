5504656faa6e0d3ecfe5aeb670934b54b8485d22
Validate and publish workflow <jbzdarkid@users.noreply.github.com>
2025-09-06 22:18:40 +0000

    Published puzzle KFMAPCPV

--- puzzle_list.js
+++ puzzle_list.js
@@ -1,4 +1,5 @@
 window.puzzleList = [
+"KFMAPCPVUnnamed Rotational Symmetry Puzzle",
 "XX2VG89LUnnamed Rotational Symmetry Puzzle",
 "SZSTY9CYUnnamed Rotational Symmetry Puzzle",
 "2AF78UG8Unnamed Horizontal Symmetry Puzzle",
