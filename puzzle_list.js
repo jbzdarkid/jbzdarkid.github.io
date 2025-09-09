f6f5bf290209728abff0a48ae3667afb7742304c
Validate and publish workflow <jbzdarkid@users.noreply.github.com>
2025-09-09 18:56:03 +0000

    Published puzzle FRGE8PRN

--- puzzle_list.js
+++ puzzle_list.js
@@ -1,4 +1,5 @@
 window.puzzleList = [
+"FRGE8PRNUnnamed Vertical Symmetry Puzzle",
 "NGZ2NLP8Unnamed Rotational Symmetry Puzzle",
 "XUEZKMT2Unnamed Rotational Symmetry Puzzle",
 "SPH2HWQWUnnamed Rotational Symmetry Puzzle",
