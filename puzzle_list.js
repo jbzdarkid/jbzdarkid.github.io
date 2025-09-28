777236b99dfaf07fb4cf003648d8beb30f4b23ab
Validate and publish workflow <jbzdarkid@users.noreply.github.com>
2025-09-28 05:36:38 +0000

    Published puzzle VACAYCLL

--- puzzle_list.js
+++ puzzle_list.js
@@ -1,4 +1,5 @@
 window.puzzleList = [
+"VACAYCLLUnnamed Rotational Symmetry Puzzle",
 "C4XXV6SLUnnamed Rotational Symmetry Puzzle",
 "HWB6HM2QUnnamed Rotational Symmetry Puzzle",
 "UGUEFZAPUnnamed Rotational Symmetry Puzzle",
