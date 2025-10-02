007d85f2b7fd00311228067f711e305f7ea72be2
Validate and publish workflow <jbzdarkid@users.noreply.github.com>
2025-10-02 23:09:21 +0000

    Published puzzle 36L2ZAUH

--- puzzle_list.js
+++ puzzle_list.js
@@ -1,4 +1,5 @@
 window.puzzleList = [
+"36L2ZAUHUnnamed Rotational Symmetry Puzzle",
 "EX45L3GMUnnamed Rotational Symmetry Puzzle",
 "32ZM5GX3Unnamed Rotational Symmetry Puzzle",
 "FJYNCKHWUnnamed Rotational Symmetry Puzzle",
