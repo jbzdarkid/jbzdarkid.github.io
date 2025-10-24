b7ea0aa231bc75ddf8c6013ef7b4cf96bf9983fb
Validate and publish workflow <jbzdarkid@users.noreply.github.com>
2025-10-24 18:45:32 +0000

    Published puzzle 77E7BXY9

--- puzzle_list.js
+++ puzzle_list.js
@@ -1,4 +1,5 @@
 window.puzzleList = [
+"77E7BXY9Squareception (SIMPLIFIED 10 SOLUTIONS)",
 "6KEJ9P7CSquareception (SIMPLIFIED 10 SOLUTIONS)",
 "RGBH3D4FSquareception (SIMPLIFIED 10 SOLUTIONS)",
 "5AKXTAKQHSP blocks",
